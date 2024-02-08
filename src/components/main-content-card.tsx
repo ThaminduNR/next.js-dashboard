482d4f06dfe9af2b0f06fa0333988b4c1ba219ef
ThaminduNR <thaminduniwantha777@gmail.com>
2024-02-08 08:20:04 +0530

    create home page

--- src/components/main-content-card.tsx
+++ src/components/main-content-card.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function MainContentCard() {
     return (
         <div className='w-full min-h-[100px] bg-slate-50 shadow-md rounded p-2 '>
-            <div></div>
+            <div>
+
+            </div>
         </div>
     )
 }
